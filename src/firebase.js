dc232f028b48f5becea479e18d2dc41fe5f1e8fa
Stefan Grigore <stefangrigore45@gmail.com>
2022-08-17 04:36:36 +0300

    added env file, reseting credentials

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMeh_WJmRrFdcbtH7zgd_Xcn2npyNYGNw",
-  authDomain: "react-auth-dev-21e1e.firebaseapp.com",
-  projectId: "react-auth-dev-21e1e",
-  storageBucket: "react-auth-dev-21e1e.appspot.com",
-  messagingSenderId: "29831670229",
-  appId: "1:29831670229:web:8c4617ec1e3e7e8956b55c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
